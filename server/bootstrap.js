786cd059d17a82c8fe4e996575cbb6d3e604d81c
Maria Hwang <mlh2169@tc.columbia.edu>
2015-12-30 19:38:42 -0500

    cosmetic and demo users

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,5 +1,28 @@
 // if the database is empty on server start, create some sample data.
 Meteor.startup(function () {
+  Meteor.users.remove({})
 
+
+  var profile =
+    {role:"student"
+    ,firstName:"Mark"
+    ,lastName:"Santolucito"}
+
+  Accounts.createUser({
+    email: "mark@yale.edu",
+    password: "mark",
+    profile: profile,
+  })
+
+  var profile2 =
+    {role:"investor"
+    ,firstName:"Fred"
+    ,lastName:"Shepard"}
+
+  Accounts.createUser({
+    email: "fred@amherst.edu",
+    password: "fred",
+    profile: profile2,
+  })
 }
 );
