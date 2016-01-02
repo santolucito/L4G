fdbb1823ae2a5a5b4aee993a8d45571cad11816a
Maria Hwang <mlh2169@tc.columbia.edu>
2016-01-01 19:48:31 -0500

    new test user accounts, see server/bootstrap.js for details

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,36 +1,48 @@
 // if the database is empty on server start, create some sample data.
+
+//first user has email "mark@amherst.edu" and password "mark"
+//others follow same style
+
 Meteor.startup(function () {
   Meteor.users.remove({})
-
-
-  var profile =
+  var profiles = [
     {role:STUDENT
     ,firstName:"Mark"
     ,lastName:"Santolucito"
     ,education:"Amherst College"
-    ,major:"Computer Science"}
+    ,major:"Computer Science"},
 
-  Accounts.createUser({
-    email: "mark@yale.edu",
-    password: "mark",
-    profile: profile,
-  })
+    {role:STUDENT
+    ,firstName:"Fiona"
+    ,lastName:"Santolucito"
+    ,education:"Weslyan University"
+    ,major:"Neuroscience"},
 
-  var profile2 =
     {role:INVESTOR
     ,firstName:"Fred"
     ,lastName:"Shepard"
     ,education:"Amherst College"
-    ,major:"Economics"}
+    ,major:"Economics"},
 
-  Accounts.createUser({
-    email: "fred@amherst.edu",
-    password: "fred",
-    profile: profile2,
-  })
+    {role:INVESTOR
+    ,firstName:"Jack"
+    ,lastName:"Sprat"
+    ,education:"Amherst College"
+    ,major:"Computer Science"}
+  ]
 
+  var makeTestUser = function(p){
+    console.log(p.firstName.toLowerCase()+"@"+
+                p.education.substr(0,p.education.indexOf(' ')).toLowerCase()+".edu");
+    Accounts.createUser({
+      email: p.firstName.toLowerCase()+"@"+
+             p.education.substr(0,p.education.indexOf(' ')).toLowerCase()+".edu",
+      password: p.firstName.toLowerCase(),
+      profile: p,
+    })
+  }
+
+  profiles.map(makeTestUser);
   console.log("DEBUG MODE: ADDED USERS");
-  console.log(profile);
-  console.log(profile2);
 }
 );
